b5c28a254d68916ef1d4ff109f19592403da486b
Mohamed Ismail <git@github.com:MAIsmail1/Portfolio.git>
2023-09-20 17:11:23 +0100

    about me start

---
+++ portfolio/src/components/Skills/skills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './skills.css';
+
+const Skills = () => {
+  return (
+    <section id="skills">
+        <p className="aboutme">
+
+        </p>
+    </section>
+  )
+}
+
+export default Skills;
