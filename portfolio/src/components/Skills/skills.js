31d084e3050a4630746361e2c63bc07a10708498
Mohamed Ismail <git@github.com:MAIsmail1/Portfolio.git>
2023-09-21 13:32:52 +0100

    added about me section

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -4,9 +4,20 @@ import './skills.css';
 const Skills = () => {
   return (
     <section id="skills">
-        <p className="aboutme">
-
-        </p>
+            <span className="title">About Me</span>
+            <br />
+            <span className="introMe">
+            Welcome to my page and here is a bit about my background. <br /> 
+            I started my career in microbiology but I decided to transition into software because, from<br />
+             the outside looking in, developers appeared to show a sense of empowerment.<br />
+            Also, I wanted to apply the knowledge I gain from learning into every project.
+            </span> <br />
+            <span className="journey">
+            After venturing around the idea of self-learning and hitting a wall, I enrolled onto the Full Stack Software Engineer Programme with
+            Bright Network. After a year in the industry, I have gained experience in working with agile teams, pair-porgramming and applying 
+            computation thinking to my code
+            </span>
+            <br /><br />
     </section>
   )
 }
