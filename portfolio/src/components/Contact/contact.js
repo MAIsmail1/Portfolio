f3db5b1c3af508bd79817e8e16906f346b86bfbb
Mohamed Ismail <git@github.com:MAIsmail1/Portfolio.git>
2023-09-26 21:12:43 +0100

    finished contact section with css

--- portfolio/src/components/Contact/contact.js
+++ portfolio/src/components/Contact/contact.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import "./contact.css";
+import {FaSquareXTwitter, FaLinkedin, FaGithub} from 'react-icons/fa6';
+import {SiGmail} from 'react-icons/si';
+
 
 const Contact = () => {
   return (
     <section id="contact">
-        
+        <div className="contactPage">
+        <h2 className="contactPageTitle">Get In Touch</h2>       
+                <span className="contactDesc">Let's talk about everything!</span>
+                <span className="contactDesc">Don't like forms? Send an email through the icon below. </span>
+            <form action="" className="contactForm">
+                        <input type = "text" className="name" 
+                        placeholder="Insert your name" />
+                        <input type = "email" className="email" 
+                        placeholder="Insert your email" />
+                        <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
+                <button type="submit" className="btn" value="send">Submit</button>
+                <ul className="iconLinks">
+                    <li>
+                        <a href="https://twitter.com/__mohamedismail" className="link"><FaSquareXTwitter /></a>
+                    </li>
+                    <li>
+                        <a href="https://linkedin.com/in/mohamed-ismail-28294a171" className="link"><FaLinkedin /></a>
+                    </li>
+                    <li>
+                        <a href="https://GitHub.com/MAIsmail1" className="link"><FaGithub /></a>
+                    </li>
+                    <li>
+                        <a href="mailto:mohamedaismail95@gmail.com" className="link"><SiGmail /></a>
+                    </li>
+                    </ul> 
+            </form>
+            </div>
     </section>
   )
 }
