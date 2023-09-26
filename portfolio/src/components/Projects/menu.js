e25953e58447ea68897f7ca58ef2937ca0758ab3
Mohamed Ismail <git@github.com:MAIsmail1/Portfolio.git>
2023-09-26 17:47:04 +0100

    added project images and styles

---
+++ portfolio/src/components/Projects/menu.js
@@ -0,0 +1,32 @@
+import Work1 from "../../assets/action.jpg";
+import Work2 from "../../assets/gomart.jpg";
+import Work3 from "../../assets/academicmind.jpg";
+import Work4 from "../../assets/portfolioimg.png";
+
+const Menu = [
+{
+  id: 1,
+  image: Work1,
+  title: "Action",
+  link: "https://github.com/MAIsmail1/CapStone_frontend",
+},
+{
+    id: 2,
+    image: Work2,
+    title: "GoMart",
+    link: "https://github.com/MAIsmail1/CapStone_frontend",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Academic Mind",
+    link: "https://github.com/MAIsmail1/CapStone_frontend",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "Portfolio",
+    link: "https://github.com/MAIsmail1/CapStone_frontend",
+  }
+]
+export default Menu
--- portfolio/src/components/Projects/projects.js
+++ portfolio/src/components/Projects/projects.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './projects.css';
+import Menu from './menu';
 
-const projects = () => {
+const Projects = () => {
+    const [items, setItems] = useState(Menu);
+
+
   return (
-    <div>projects</div>
+    <section className="workContainer" id="work">
+        <h2 className="sectionTitle">Projects</h2>
+        <br />
+        <div className="workContainerGrid">
+            {items.map((elem) => {
+                const{id, image, title, link} = elem;
+                return(
+                    <div className="workCard" key={id}>
+                        <div className="workThumbnail">
+                            <img src={image} alt="images" className="workImg" />
+                            <div className="workMask"></div>
+                            </div>
+                            <h3 className="workTitle">Action</h3>
+                            <a href="https://github.com/MAIsmail1/CapStone_frontend" className="workButton">
+                                <i className="iconLink"></i>
+                            </a>
+                        </div>
+                )
+            })}
+        </div>
+    </section>
   )
 }
 
-export default projects
+export default Projects
