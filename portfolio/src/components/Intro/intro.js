22d330796b49b8a4f8402df1d730d930c7ed8149
Mohamed Ismail <git@github.com:MAIsmail1/Portfolio.git>
2023-09-29 22:58:45 +0100

    css changes

--- portfolio/src/components/Intro/intro.js
+++ portfolio/src/components/Intro/intro.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import './intro.css';
 import bg from '../../assets/mypicture.png'
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 import btnImg from '../../assets/hireme.png';
 import resume from '../../assets/Resume (2).pdf';
 import { TypeAnimation } from 'react-type-animation';
 
 const Intro = () => {
   return (
-    <section id = "intro" >
-        <div className="introContent" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">
-            <span className="hello">Hello,</span>
-            <span className="IntroText">I'm <span className="introName">Mohamed</span> <br /> 
-            <TypeAnimation
+    <section id="intro" >
+      <div className="introContent" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">
+        <span className="hello">Hello,</span>
+        <span className="IntroText">I'm <span className="introName">Mohamed</span> <br />
+          <TypeAnimation
             sequence={[
               'Software Developer',
               2500,
               'Web Designer',
               2500
             ]}
+            style={{ fontSize: '1.3rem' }}
             repeat={Infinity}
-            />
-            </span>
-            <p className="introPara">I am a skilled and passionate software developer with  <br /> experience in creating websites and web applications</p>
+          />
+        </span>
+        <p className="introPara">I am a skilled and passionate software developer with  <br /> experience in creating websites and web applications</p>
         <a href={resume} download="Resume">
-        <button className="btn"><img src={btnImg} alt="" className="btnImg"/>Hire Me</button>
+          <button className="btn"><img src={btnImg} alt="" className="btnImg" />Hire Me</button>
         </a>
-        <div>
-        <img src = {bg} alt = "Profile" className="bg" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true"/>
-        </div>
-        </div>
-        
+      </div>
+      <div>
+        <img src={bg} alt="Profile" className="bg hideImgSmall" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" />
+      </div>
     </section>
   )
 }
